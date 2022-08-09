d791ffb5e673ddfd832091432f115e161fcd1ef4
krish-199 <68241105+krish-199@users.noreply.github.com>
2022-08-09 23:57:26 +0530

    changing route

--- pages/index.js
+++ pages/index.js
@@ -37,10 +37,12 @@ export default function Home({ isConnected }) {
           </h2>
         )}
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <Link href="visit">
+            <a className={styles.card}>
+              <h2>Patient Visit &rarr;</h2>
+              <p>Record new Patient Visit</p>
+            </a>
+          </Link>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
