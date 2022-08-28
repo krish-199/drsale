6703a550224683355e92c14a35c84a9c25e71e00
krish-199 <68241105+krish-199@users.noreply.github.com>
2022-08-28 21:51:49 +0530

    adding error handling

--- pages/api/searchUser.js
+++ pages/api/searchUser.js
@@ -1,4 +1,5 @@
 import clientPromise from "@/lib/mongodb";
+import httpStatus from "http-status";
 
 export default async (req, res) => {
   const client = await clientPromise;
@@ -8,9 +9,16 @@ export default async (req, res) => {
   let re = new RegExp(body.searchValue);
   const users = await db
     .collection("patients")
-    .find({ [body.searchField]: re })
+    .find({ [body.searchField]: re }, function (err, result) {
+      if (err) {
+        console.log("err", err);
+        res
+          .status(httpStatus["400_MESSAGE"])
+          .send("Error Storing Visit details!!");
+      }
+    })
     .limit(50)
     .toArray();
-    
-  res.json(users);
+
+  res.status(httpStatus.OK).json(users);
 };
