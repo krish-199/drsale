54d87845cfa7b1ac00cd4f93dde29423571788d5
krish-199 <68241105+krish-199@users.noreply.github.com>
2022-08-28 22:25:20 +0530

    checking logged in status before page access

--- pages/new-patient.js
+++ pages/new-patient.js
@@ -1,9 +1,26 @@
 import Profile from "@/components/profile";
-import { Box, useColorModeValue } from "@chakra-ui/react";
-import React from "react";
+import useUser from "@/lib/useUser";
+import { useRouter } from "next/router";
+import { Box, useColorModeValue, useToast } from "@chakra-ui/react";
+import React, { useEffect } from "react";
 import styles from "../styles/visit.module.css";
 
-const NewPatient = (props) => {
+const NewPatient = () => {
+  const { user } = useUser();
+  const toast = useToast();
+  const router = useRouter();
+
+  useEffect(() => {
+    user?.isLoggedIn
+      ? null
+      : toast({
+          title: "Not logged in",
+          description: "You are logged in. Please login to continue",
+          status: "error",
+          duration: 3000,
+          isClosable: false,
+        }) && router.push("/login");
+  }, [user, router, toast]);
   return (
     <div className={styles.contianer}>
       <Box bg={useColorModeValue("gray.50", "inherit")} p={10}>
--- pages/visit.js
+++ pages/visit.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useUser from "@/lib/useUser";
+import { useRouter } from "next/router";
 import VisitBox from "@/components/visit-box";
-import { Box, useColorModeValue } from "@chakra-ui/react";
+import { Box, useColorModeValue, useToast } from "@chakra-ui/react";
 import styles from "../styles/visit.module.css";
 
 const Visit = () => {
+  const { user } = useUser();
+  const toast = useToast();
+  const router = useRouter();
+
+  useEffect(() => {
+    user?.isLoggedIn
+      ? null
+      : toast({
+          title: "Not logged in",
+          description: "You are logged in. Please login to continue",
+          status: "error",
+          duration: 3000,
+          isClosable: false,
+        }) && router.push("/login");
+  }, [user, router, toast]);
+
   return (
     <div className={styles.contianer}>
       <Box bg={useColorModeValue("gray.50", "inherit")} p={10}>
