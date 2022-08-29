309c0c9ffa7e3686d9e301c9b0487cdba9047fba
krish-199 <68241105+krish-199@users.noreply.github.com>
2022-08-29 21:53:40 +0530

    moving login popup code

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,46 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import React, { useEffect, useState, useRef } from "react";
+import useUser from "@/lib/use-user";
+import { useRouter } from "next/router";
+import { ChakraProvider, useToast } from "@chakra-ui/react";
+import LoginModal from "@/components/login-modal";
 
 function MyApp({ Component, pageProps }) {
+  const { user } = useUser();
+  const toast = useToast();
+  const router = useRouter();
+  const isMounted = useRef(false);
+
+  const [loginState, setLoginState] = useState(false);
+
+  useEffect(() => {
+    if (isMounted.current)
+      user?.isLoggedIn
+        ? toast({
+            title: "Logged In",
+            status: "success",
+            duration: 2000,
+            isClosable: true,
+          }) && setLoginState(false)
+        : toast({
+            title: "Not logged in",
+            description: "You are logged in. Please login to continue",
+            status: "error",
+            duration: 3000,
+            isClosable: false,
+          }) && setLoginState(true);
+    isMounted.current = true;
+  }, [user]);
+
+  const loginNavigate = () => router.push("/login");
+
   return (
     <ChakraProvider>
       <Component {...pageProps} />
+      <LoginModal
+        isOpen={loginState}
+        action={loginNavigate}
+        onClose={loginNavigate}
+      />
     </ChakraProvider>
   );
 }
--- pages/visit.js
+++ pages/visit.js
@@ -1,47 +1,14 @@
-import React, { useEffect, useState } from "react";
-import useUser from "@/lib/use-user";
-import { useRouter } from "next/router";
+import React from "react";
 import VisitBox from "@/components/visit-box";
-import { Box, useColorModeValue, useToast } from "@chakra-ui/react";
+import { Box, useColorModeValue } from "@chakra-ui/react";
 import styles from "../styles/visit.module.css";
-import LoginModal from "@/components/login-modal";
 
 const Visit = () => {
-  const { user } = useUser();
-  const toast = useToast();
-  const router = useRouter();
-
-  const [loginState, setLoginState] = useState(false);
-
-  useEffect(() => {
-    user?.isLoggedIn
-      ? toast({
-          title: "Logged In",
-          status: "success",
-          duration: 2000,
-          isClosable: true,
-        }) && setLoginState(false)
-      : toast({
-          title: "Not logged in",
-          description: "You are logged in. Please login to continue",
-          status: "error",
-          duration: 3000,
-          isClosable: false,
-        }) && setLoginState(true);
-  }, [user]);
-
-  const loginNavigate = () => router.push("/login");
-
   return (
     <div className={styles.contianer}>
       <Box bg={useColorModeValue("gray.50", "inherit")} p={10}>
         <VisitBox />
       </Box>
-      <LoginModal
-        isOpen={loginState}
-        action={loginNavigate}
-        onClose={loginNavigate}
-      />
     </div>
   );
 };
