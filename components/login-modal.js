f89a3bb1ffec5e85fbab9d2d1b6fb09bd3121e36
krish-199 <68241105+krish-199@users.noreply.github.com>
2022-08-29 17:53:04 +0530

    showing login modal if not logged in

---
+++ components/login-modal.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+
+const LoginModal = (props) => {
+  const router = useRouter();
+  const { isOpen, action, onClose } = props;
+  return (
+    <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>"Please login before continuing"</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <p>You are not logged in....</p>
+          <p>Press below button to navigate to login page</p>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="red" mr={3} onClick={action}>
+            "To Login..."
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default LoginModal;
--- pages/new-patient.js
+++ pages/new-patient.js
@@ -2,30 +2,47 @@ import Profile from "@/components/profile";
 import useUser from "@/lib/use-user";
 import { useRouter } from "next/router";
 import { Box, useColorModeValue, useToast } from "@chakra-ui/react";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/visit.module.css";
+import LoginModal from "@/components/login-modal";
 
 const NewPatient = () => {
   const { user } = useUser();
   const toast = useToast();
   const router = useRouter();
 
+  const [loginState, setLoginState] = useState(false);
+
   useEffect(() => {
     user?.isLoggedIn
-      ? null
+      ? setLoginState(false) &&
+        toast({
+          title: "Logged In",
+          status: "success",
+          duration: 2000,
+          isClosable: true,
+        })
       : toast({
           title: "Not logged in",
           description: "You are logged in. Please login to continue",
           status: "error",
           duration: 3000,
           isClosable: false,
-        }) && router.push("/login");
-  }, [user, router, toast]);
+        }) && setLoginState(true);
+  }, [user]);
+
+  const loginNavigate = () => router.push("/login");
+
   return (
     <div className={styles.contianer}>
       <Box bg={useColorModeValue("gray.50", "inherit")} p={10}>
         <Profile />
       </Box>
+      <LoginModal
+        isOpen={loginState}
+        action={loginNavigate}
+        onClose={loginNavigate}
+      />
     </div>
   );
 };
