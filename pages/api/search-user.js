77c77a7b958f10183a5569f850210842f7c48480
krishnaB <krishna@afreespace.com>
2022-11-14 23:33:12 +0530

    fixing no user api response

--- pages/api/search-user.js
+++ pages/api/search-user.js
@@ -2,23 +2,27 @@ import clientPromise from "@/lib/mongodb";
 import httpStatus from "http-status";
 
 const searchUser = async (req, res) => {
-  const client = await clientPromise;
+  try {
+    const client = await clientPromise;
 
-  const db = client.db();
-  const body = JSON.parse(req.body);
-  let re = new RegExp(body.searchValue);
-  const users = await db
-    .collection("patients")
-    .find({ [body.searchField]: re })
-    .limit(50)
-    .toArray();
+    const db = client.db();
+    const body = JSON.parse(req.body);
+    let re = new RegExp(body.searchValue);
+    const users = await db
+      .collection('patients')
+      .find({ [body.searchField]: re })
+      .limit(50)
+      .toArray();
 
-  if (users.length === 0) {
-    console.log("Patient donesn't exist");
-    res.status(httpStatus["400_MESSAGE"]).send("Patient doesn't exist");
+    if (users.length === 0) {
+      console.log("Patient doesn't exist");
+      res.status(httpStatus.BAD_REQUEST).json("Patient doesn't exist");
+    } else {
+      res.status(httpStatus.OK).json(users);
+    }
+  } catch (error) {
+    console.error(`Error While fetching ${error}`);
   }
-
-  res.status(httpStatus.OK).json(users);
 };
 
 export default searchUser;
